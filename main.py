bca5ab80342fb27dfb9ecfc7338c92347521af01
E. Evstafiev <ee345@cam.ac.uk>
2024-09-29 11:15:06 +0100

    fead: add requests limit

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 import base64
-import urllib.parse
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
 app = FastAPI()
+limiter = Limiter(key_func=get_remote_address, storage_uri="memory://")
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,37 +17,31 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
+# Register the rate limit exceeded error handler
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 @app.post("/encode-url/")
+@limiter.limit("50/minute")
 async def encode_url(request: Request):
     json_data = await request.json()
     url = json_data.get("url")
     if not url:
         raise HTTPException(status_code=400, detail="URL parameter is missing.")
 
-    # Base64 encode the URL
     encoded_url = base64.urlsafe_b64encode(url.encode("utf-8")).decode("utf-8")
-
-    # Generate a new endpoint to access it
     response_url = request.url_for("redirect_to_url", encoded_url=encoded_url)
-
     return {"encoded_url": response_url}
 
-
 @app.get("/redirect/{encoded_url}")
-async def redirect_to_url(encoded_url: str):
-    # Decode the base64 URL
+@limiter.limit("60/minute")
+async def redirect_to_url(request: Request, encoded_url: str):
     try:
         decoded_url = base64.urlsafe_b64decode(encoded_url.encode("utf-8")).decode("utf-8")
     except Exception as e:
         raise HTTPException(status_code=400, detail="Invalid URL encoding.")
-
-    # Redirect to the original URL
     return RedirectResponse(url=decoded_url)
-
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8080)
